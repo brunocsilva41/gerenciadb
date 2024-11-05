8c71e018de9d55acebdd061967c59bb78ac55e8f
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-05 00:15:07 -0300

    troca de banco para aws

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import sqlite3 from 'sqlite3';
-
 dotenv.config();
 
 const app = express();
@@ -16,13 +14,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Conexão com o banco de dados SQLite
-const db = new sqlite3.Database('./db/usuarios_banco.db', (err) => {
-    if (err) {
-        console.error('Erro ao conectar com o banco de dados SQLite:', err.message);
-    } else {
-        console.log('Conectado ao banco de dados SQLite!');
-    }
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
+db.connect(err => {
+    if (err) throw err;
+    console.log('Conectado ao banco de dados MySQL!');
+});
+
 
 // Importa o router de usuários
 import userRoutes from './routes/users.js';
