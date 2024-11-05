6841150f4701de3e41a2172af24b49799eea2cf1
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-05 00:58:39 -0300

    ajustes

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ app.use(bodyParser.json());
 
 // Conexão com o banco de dados MySQL
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    host: process.env.db_host,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_name,
+    port: process.env.db_port || 3306
 });
 
 db.connect(err => {
