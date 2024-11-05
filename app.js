74d0113d951bfc07a9f661d68fbdf3027c9ae9df
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-05 00:41:02 -0300

    fixx--

--- app.js
+++ app.js
@@ -20,11 +20,15 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT || 3306
 });
+
 db.connect(err => {
-    if (err) throw err;
-    console.log('Conectado ao banco de dados MySQL!');
+    if (err) {
+        console.error('Erro ao conectar com o banco de dados MySQL:', err.message);
+    } else {
+        console.log('Conectado ao banco de dados MySQL no AWS RDS!');
+    }
 });
 app.post('/api/usuarios', (req, res) => {
     const sql = 'SELECT * FROM usuarios';
