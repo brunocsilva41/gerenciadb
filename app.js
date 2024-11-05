2e1d23b97236b8c077bc130cf551ef73f2424ebe
Bruno Silva <84159328+brunocsilva41@users.noreply.github.com>
2024-11-04 21:04:54 -0300

    adding router --

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
+// app.js
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose(); // Substituir por sqlite3
+const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const cors = require('cors');
 const { body, validationResult } = require('express-validator');
 require('dotenv').config();
-const cors = require('cors');
-const app = express();
 
+const app = express();
 app.use(cors({
     origin: 'http://localhost:8000'
 }));
 
-const port = process.env.DB_PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -24,62 +23,12 @@ const db = new sqlite3.Database('./db/novo_banco.db', (err) => {
     }
 });
 
-// Rota para criar conta
-app.post('/criar-conta', [
-    body('name').isLength({ min: 1 }).trim().escape(),
-    body('email').isEmail().normalizeEmail(),
-    body('password').isLength({ min: 3 }).trim().escape()
-], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    const { name, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const sql = `INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)`;
-    db.run(sql, [name, email, hashedPassword], function (err) {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.status(201).json({ message: 'Conta criada com sucesso!' });
-    });
-});
+// Importa o router de usuários
+const userRoutes = require('./routes/users')(db); // Passa o `db` para o router de usuários
+app.use('/api/users', userRoutes); // Define o prefixo para as rotas de usuários
 
-// Rota para login
-app.post('/login-conta', [
-    body('email').isEmail().normalizeEmail(),
-    body('password').isLength({ min: 1 }).trim().escape()
-], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    const { email, password } = req.body;
-    const sql = `SELECT * FROM usuarios WHERE email = ?`;
-    db.get(sql, [email], async (err, user) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        if (user) {
-            const match = await bcrypt.compare(password, user.senha);
-            if (match) {
-                res.status(200).json({
-                    message: 'Login bem-sucedido',
-                    userId: user.id,
-                    userName: user.nome,
-                    userEmail: user.email
-                });
-            } else {
-                res.status(401).json({ message: 'Senha incorreta' });
-            }
-        } else {
-            res.status(401).json({ message: 'Usuário não encontrado' });
-        }
-    });
-});
-
-// Iniciar o servidor HTTP
-app.listen(port, (err) => {
-    if (err) console.error(err);
-    console.log(`Servidor rodando em ${baseURL}:${port}`);
+// Inicializa o servidor
+const port = process.env.DB_PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
